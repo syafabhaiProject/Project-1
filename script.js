2b015441676f2838edb9954f2aa73a20c4581cde
syafabhaiProject <syafabhai@gmail.com>
2026-01-23 10:24:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const panel = document.getElementById("slidePanel");
+
+function openPanel() {
+    panel.classList.add("active");
+}
+
+function closePanel() {
+    panel.classList.remove("active");
+}
 
